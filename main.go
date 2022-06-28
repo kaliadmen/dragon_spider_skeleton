0c6633ae76c4e6336f40838746c81914d71cad41
kaliadmen <kaliadmen@gmail.com>
2022-06-28 17:34:22 -0400

    ADD Graceful shutdown of application

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	dragonSpider "github.com/kaliadmen/dragon_spider"
 	"${APP_NAME}/data"
 	"${APP_NAME}/handlers"
 	"${APP_NAME}/middleware"
+	dragonSpider "github.com/kaliadmen/dragon_spider"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 //application holds data all the needed for Dragon Spider App
@@ -13,10 +17,34 @@ type application struct {
 	Handlers   *handlers.Handlers
 	Models     data.Models
 	Middleware *middleware.Middleware
+	wg         sync.WaitGroup
 }
 
 func main() {
 	ds := initApplication()
 
-	ds.App.ListenAndServe()
+	go ds.listenForShutdown()
+
+	err := ds.App.ListenAndServe()
+	ds.App.ErrorLog.Println(err)
+}
+
+func (a *application) shutdown() {
+	//put clean up task here
+
+	//block until wait group is empty
+	a.wg.Wait()
+
+}
+
+func (a *application) listenForShutdown() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	s := <-quit
+
+	a.App.InfoLog.Println(s.String())
+	a.shutdown()
+
+	os.Exit(0)
 }
