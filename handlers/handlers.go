63e7d545dfc2e614bc28a7fe5cd811349b6474d0
kaliadmen <kaliadmen@gmail.com>
2022-06-28 17:26:42 -0400

    ADD Comments to handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
 )
 
+//Handlers is the type for handlers, and gives access to DragonSpider and models
 type Handlers struct {
 	App    *dragonSpider.DragonSpider
 	Models data.Models
 }
 
+//Home is the handler to render a home page
 func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
 	err := h.render(w, r, "home", nil, nil)
 	if err != nil {
